825f393d84a6f7af97acbf35777d37974a0705ed
nemisgerry <gerrymaths@gmail.com>
2017-05-08 19:26:43 -0500

    Actually the script print all subset from a given set

--- subsets.py
+++ subsets.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 import sys
+from itertools import izip
 
-def decimal_to_list_binary(n):
-	return map(int,list('{0:0b}'.format(n)))
+def decimal_to_list_binary(n,t):
+	return map(int,str('{0:0'+str(t)+'b}').format(n))
 
 def compress(data, filter):
-    return (d for d, s in izip(data, selectors) if s)
-    
+    return (d for d, s in izip(data, filter) if s)
+
 if __name__ == '__main__':
 	print 'Give me a set of numbers:'
 	original_set = raw_input()
 	original_set = map(int,original_set.split())
 	n = len(original_set)
-	for i in range((2**n)-1):
-		print bin(i)
-	print (original_set,n)
+	subsets=[]
+	for i in range((2**n)):
+		filt = decimal_to_list_binary(i,n)
+		a= compress(original_set,filt)
+		subsets.append(list(a))
+	for subset in subsets:
+		print (subset)
